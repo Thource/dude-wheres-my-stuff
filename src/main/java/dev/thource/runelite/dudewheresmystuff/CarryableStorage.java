70c3d9dd36735341ff9228a6f8922efa174e3b14
Thource <reyitgex@gmail.com>
2022-04-29 23:35:45 +0100

    Fix ammo not appearing in the correct place in unsorted equipment

--- src/main/java/dev/thource/runelite/dudewheresmystuff/CarryableStorage.java
+++ src/main/java/dev/thource/runelite/dudewheresmystuff/CarryableStorage.java
@@ -2,6 +2,7 @@ package dev.thource.runelite.dudewheresmystuff;
 
 import lombok.Getter;
 import net.runelite.api.Client;
+import net.runelite.api.EquipmentInventorySlot;
 import net.runelite.api.events.ItemContainerChanged;
 import net.runelite.client.game.ItemManager;
 
@@ -17,12 +18,18 @@ public class CarryableStorage extends Storage<CarryableStorageType> {
     boolean updated = super.onItemContainerChanged(itemContainerChanged);
 
     if (type == CarryableStorageType.EQUIPMENT && updated) {
-      // move ammo into the correct place
-      ItemStack ammo = items.remove(11);
-      items.add(3, ammo);
+      ItemStack empty = new ItemStack(-1, "empty", 1, 0, 0, false);
+
+      if (items.size() > 13) {
+        // move ammo into the correct place if the slot exists
+        ItemStack ammo = items.remove(EquipmentInventorySlot.AMMO.getSlotIdx());
+        items.add(3, ammo);
+      } else {
+        items.add(3, empty);
+      }
+      items.remove(12); // remove empty space between boots and ring
 
       // pad it out to fit the 4 wide grid
-      ItemStack empty = new ItemStack(-1, "empty", 1, 0, 0, false);
       items.add(0, empty);
       items.add(2, empty);
       items.add(3, empty);
