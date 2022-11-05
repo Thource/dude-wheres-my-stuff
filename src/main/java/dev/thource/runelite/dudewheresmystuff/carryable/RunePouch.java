3c23f4003e0713605dfd111ecb2a3fbbfc2905ae
Peace Bear0 <peace.bear0@gmail.com>
2022-11-05 11:59:09 -0700

    Add support for divine rune pouch

--- src/main/java/dev/thource/runelite/dudewheresmystuff/carryable/RunePouch.java
+++ src/main/java/dev/thource/runelite/dudewheresmystuff/carryable/RunePouch.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import net.runelite.api.EnumComposition;
 import net.runelite.api.EnumID;
 import net.runelite.api.Varbits;
+import java.util.Arrays;
 
 /**
  * RunePouch is responsible for tracking how many runes the player has stored in their rune pouch.
@@ -15,13 +16,23 @@ public class RunePouch extends CarryableStorage {
 
   private static final String EMPTY = "empty";
 
-  private int rune1Type;
-  private int rune2Type;
-  private int rune3Type;
+  private static int maxRuneTypes = 4;
+  private static final int[] runeTypeVarbits = {
+          Varbits.RUNE_POUCH_RUNE1,
+          Varbits.RUNE_POUCH_RUNE2,
+          Varbits.RUNE_POUCH_RUNE3,
+          Varbits.RUNE_POUCH_RUNE4,
+  };
 
-  private int rune1Quantity;
-  private int rune2Quantity;
-  private int rune3Quantity;
+  private static final int[] runeQuantityVarbits = {
+          Varbits.RUNE_POUCH_AMOUNT1,
+          Varbits.RUNE_POUCH_AMOUNT2,
+          Varbits.RUNE_POUCH_AMOUNT3,
+          Varbits.RUNE_POUCH_AMOUNT4,
+  };
+
+  private int runeTypes[] = new int[maxRuneTypes];
+  private int runeQuantities[] = new int[maxRuneTypes];
 
   RunePouch(DudeWheresMyStuffPlugin plugin) {
     super(CarryableStorageType.RUNE_POUCH, plugin);
@@ -29,29 +40,20 @@ public class RunePouch extends CarryableStorage {
 
   @Override
   public boolean onVarbitChanged() {
-    int newRune1Type = plugin.getClient().getVarbitValue(Varbits.RUNE_POUCH_RUNE1);
-    int newRune2Type = plugin.getClient().getVarbitValue(Varbits.RUNE_POUCH_RUNE2);
-    int newRune3Type = plugin.getClient().getVarbitValue(Varbits.RUNE_POUCH_RUNE3);
-    int newRune1Quantity = plugin.getClient().getVarbitValue(Varbits.RUNE_POUCH_AMOUNT1);
-    int newRune2Quantity = plugin.getClient().getVarbitValue(Varbits.RUNE_POUCH_AMOUNT2);
-    int newRune3Quantity = plugin.getClient().getVarbitValue(Varbits.RUNE_POUCH_AMOUNT3);
-
-    if (newRune1Type == rune1Type
-        && newRune2Type == rune2Type
-        && newRune3Type == rune3Type
-        && newRune1Quantity == rune1Quantity
-        && newRune2Quantity == rune2Quantity
-        && newRune3Quantity == rune3Quantity) {
+    int[] newTypes = new int[maxRuneTypes];
+    int[] newQuantities = new int[maxRuneTypes];
+    for (int i=0; i<maxRuneTypes; i++) {
+      newTypes[i] = plugin.getClient().getVarbitValue(runeTypeVarbits[i]);
+    }
+    for (int i=0; i<maxRuneTypes; i++) {
+      newQuantities[i] = plugin.getClient().getVarbitValue(runeQuantityVarbits[i]);
+    }
+    if (Arrays.equals(newQuantities, runeQuantities) && Arrays.equals(newTypes, runeTypes)) {
       return false;
     }
 
-    rune1Type = newRune1Type;
-    rune2Type = newRune2Type;
-    rune3Type = newRune3Type;
-
-    rune1Quantity = newRune1Quantity;
-    rune2Quantity = newRune2Quantity;
-    rune3Quantity = newRune3Quantity;
+    runeQuantities = newQuantities;
+    runeTypes = newTypes;
 
     refreshItems();
 
@@ -62,26 +64,14 @@ public class RunePouch extends CarryableStorage {
     items.clear();
 
     EnumComposition runepouchEnum = plugin.getClient().getEnum(EnumID.RUNEPOUCH_RUNE);
-    int rune1Id = runepouchEnum.getIntValue(rune1Type);
-    int rune2Id = runepouchEnum.getIntValue(rune2Type);
-    int rune3Id = runepouchEnum.getIntValue(rune3Type);
 
-    if (rune1Id != -1) {
-      items.add(new ItemStack(rune1Id, rune1Quantity, plugin));
-    } else {
-      items.add(new ItemStack(-1, EMPTY, 1, 0, 0, false));
-    }
-
-    if (rune2Id != -1) {
-      items.add(new ItemStack(rune2Id, rune2Quantity, plugin));
-    } else {
-      items.add(new ItemStack(-1, EMPTY, 1, 0, 0, false));
-    }
-
-    if (rune3Id != -1) {
-      items.add(new ItemStack(rune3Id, rune3Quantity, plugin));
-    } else {
-      items.add(new ItemStack(-1, EMPTY, 1, 0, 0, false));
+    for (int i=0; i<maxRuneTypes; i++) {
+      int runeId = runepouchEnum.getIntValue(runeTypes[i]);
+      if (runeId != -1) {
+        items.add(new ItemStack(runeId, runeQuantities[i], plugin));
+      } else {
+        items.add(new ItemStack(-1, EMPTY, 1, 0, 0, false));
+      }
     }
   }
 
@@ -89,12 +79,9 @@ public class RunePouch extends CarryableStorage {
   public void reset() {
     super.reset();
 
-    rune1Type = 0;
-    rune2Type = 0;
-    rune3Type = 0;
-
-    rune1Quantity = 0;
-    rune2Quantity = 0;
-    rune3Quantity = 0;
+    for (int i=0; i<maxRuneTypes; i++) {
+      runeTypes[i] = 0;
+      runeQuantities[i] = 0;
+    }
   }
 }
