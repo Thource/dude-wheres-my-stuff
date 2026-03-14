5690075adc01ee57537de962752bc11d5af4544f
Adam Sharp <me@thource.dev>
2026-03-14 00:18:15 +0000

    Bump to 2.11.3

--- src/main/java/dev/thource/runelite/dudewheresmystuff/DudeWheresMyStuffPlugin.java
+++ src/main/java/dev/thource/runelite/dudewheresmystuff/DudeWheresMyStuffPlugin.java
@@ -250,7 +250,7 @@ public class DudeWheresMyStuffPlugin extends Plugin {
 
       var lastVersion = configManager.getConfiguration(DudeWheresMyStuffConfig.CONFIG_GROUP,
           "version");
-      configManager.setConfiguration(DudeWheresMyStuffConfig.CONFIG_GROUP, "version", "2.11.2");
+      configManager.setConfiguration(DudeWheresMyStuffConfig.CONFIG_GROUP, "version", "2.11.3");
       // Delete all lost boats from v2.11.1 and before
       if (lastVersion == null) {
         getProfilesWithData()
